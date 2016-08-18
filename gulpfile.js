4070c1fc4f1fc214648b9da1e3aadf8501afe579
Joshua Efiong <joshefiong@gmail.com>
2016-08-18 22:59:55 +0100

    Added browserify to the gulp 'default' task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
 
 gulp.task('default', function() {
-  return tsProject.src()
-    .pipe(ts(tsProject))
-    .js.pipe(gulp.dest('dist'));
+  return browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts', 'typings/index.d.ts', 'typings/main.d.ts'],
+    cache: {},
+    packageCache: {},
+  })
+  .plugin(tsify)
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('dist'));
 });
