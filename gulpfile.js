b5ec969083e82ff940f0c56e681e8c73180a9af7
Joshua Efiong <joshefiong@gmail.com>
2016-08-19 01:56:22 +0100

    Wrote gulp tasks to build the demo page to the 'build' directory.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var tsify = require('tsify');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
   return browserify({
@@ -15,4 +16,29 @@ gulp.task('default', function() {
   .bundle()
   .pipe(source('bundle.js'))
   .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy', function() {
+  gulp.src('./demo/index.html')
+    .pipe(gulp.dest('build'));
+})
+
+gulp.task('demo-scss', function() {
+  return gulp.src(['./demo/**/*.{scss,sass}'])
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('demo', ['copy', 'demo-scss'], function() {
+  return browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['demo/index.tsx'],
+    cache: {},
+    packageCache: {},
+  })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('demo.js'))
+    .pipe(gulp.dest('./build'));
 });
