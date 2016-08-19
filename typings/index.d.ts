61767e03cafd64bdbf34727e3564ec6225c70307
Joshua Efiong <joshefiong@gmail.com>
2016-08-19 01:35:07 +0100

    Added bluebird Promise library typings.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/bluebird/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
