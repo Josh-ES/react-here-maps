e81a3dc56ccc47e5a3d43077348eb5c2afc9fb0a
Joshua Efiong <joshefiong@gmail.com>
2016-08-19 01:56:03 +0100

    Created a demo page, with stylesheet and scripts.

---
+++ demo/demo.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import HEREMap from '../src/main.ts';
+
+export default class App extends React.Component<any, any> {
+    render() {
+        const center = {
+            lat: ***REMOVED***,
+            lng: ***REMOVED***,
+        };
+
+        return (
+            <div className="content">
+                <HEREMap center={center} zoom={10} appId='***REMOVED***' appCode='***REMOVED***' />
+            </div>
+        )
+    }
+}
---
+++ demo/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './demo.tsx';
+
+ReactDOM.render(<App />, document.getElementById('rapp'));
