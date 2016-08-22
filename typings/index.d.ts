9689c1ca264aeefea624dfb4ef0cd7de31ee7d59
Joshua Efiong <joshefiong@gmail.com>
2016-08-22 14:40:02 +0100

    Add chai to the project.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/bluebird/index.d.ts" />
+/// <reference path="globals/chai/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
