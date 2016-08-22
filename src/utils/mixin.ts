c731896e747f971521d5d63108432168b09b476b
Joshua Efiong <joshefiong@gmail.com>
2016-08-22 13:16:43 +0100

    Wrote a mixin-generator utility.

---
+++ src/utils/mixin.ts
@@ -0,0 +1,45 @@
+export function mixin(behaviour: any, sharedBehaviour: any = {}) {
+    // these keys reflect the behaviour that is to be attached to class instances
+    const instanceKeys = Reflect.ownKeys(behaviour);
+    // these keys reflect static behaviour
+    const sharedKeys = Reflect.ownKeys(sharedBehaviour);
+    const typeTag = Symbol('isA');
+
+    function _mixin(workingClass: any) {
+        // attach instance-oriented behaviour
+        for (let property of instanceKeys) {
+            Object.defineProperty(
+                workingClass.prototype,
+                property,
+                { value: behaviour[property] }
+            );
+        }
+
+        Object.defineProperty(workingClass.prototype, typeTag, { value: true });
+    }
+
+    // attach static behaviour
+    for (let property of sharedKeys) {
+        Object.defineProperty(
+            _mixin,
+            property,
+            {
+                value: sharedBehaviour[property],
+                enumerable: sharedBehaviour.propertyIsEnumerable(property),
+            }
+        );
+    }
+
+    // this allows you to use 'instanceof' on an object that uses a mixin
+    Object.defineProperty(
+        _mixin,
+        Symbol.hasInstance,
+        {
+            value: (instance: any) => !!instance[typeTag],
+        }
+    );
+
+    return _mixin;
+}
+
+export default mixin;
