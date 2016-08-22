41fe80fe6d1a10587136d3fa4f7c0cfe7d685a31
Joshua Efiong <joshefiong@gmail.com>
2016-08-22 14:33:23 +0100

    Add codeclimate and mocha support.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="globals/bluebird/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
-
 /// <reference path="packages/heremaps/heremaps.d.ts" />
