ca350e12a883ec54f18dd2bf4c30266637dfe5c9
Joshua Efiong <joshefiong@gmail.com>
2016-08-22 22:29:34 +0100

    Added a test for the number of script elements on the page.

--- test/HEREMap.tests.tsx
+++ test/HEREMap.tests.tsx
@@ -1,8 +1,12 @@
 import HEREMap from "../src/HEREMap";
+import getScriptMap from "../src/utils/get-script-map";
 import * as chai from "chai";
-import * as sinon from "sinon";
+import * as cheerio from "cheerio";
 import { mount, shallow } from "enzyme";
 import * as React from "react";
+import * as sinon from "sinon";
+
+declare var global: any;
 
 describe("<HEREMap />", () => {
 
@@ -17,6 +21,15 @@ describe("<HEREMap />", () => {
         chai.expect(HEREMap.prototype.componentDidMount).to.have.property("callCount", 1);
         // make sure we restore the original method at the end of the test, removing the spy
         didMountSpy.restore();
+    });
+
+    it("should generate all the necessary script elements within the document", () => {
+        const html = global.document.documentElement.outerHTML;
+        const $ = cheerio.load(html);
+        const scriptMap = getScriptMap();
+        // check the length of the script map is equal to the number of script elements on the page
+        // - we can do this as there are no other scripts on the page during testing
+        chai.expect($("script").length).to.equal(Object.keys(scriptMap).length);
     });
 
 });
