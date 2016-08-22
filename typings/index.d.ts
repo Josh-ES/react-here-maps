fddcff556efbc1b0fd38f35c1c854a61288d1b3d
Joshua Efiong <joshefiong@gmail.com>
2016-08-22 14:44:31 +0100

    Add enzyme to the project.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/bluebird/index.d.ts" />
 /// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/enzyme/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
