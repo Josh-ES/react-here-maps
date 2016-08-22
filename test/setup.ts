2dde5f25c592d82d66d9ef247e3323b1414153f0
Joshua Efiong <joshefiong@gmail.com>
2016-08-22 21:37:18 +0100

    Cast the property index expression as any to prevent TS error.

--- test/setup.ts
+++ test/setup.ts
@@ -1,21 +1,21 @@
-import * as jsdom from 'jsdom';
+import * as jsdom from "jsdom";
 
 declare var global: any;
 
-const exposedProperties = ['window', 'navigator', 'document'];
+const exposedProperties = ["window", "navigator", "document"];
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const doc = jsdom.jsdom("<!doctype html><html><body></body></html>");
 
 global.document = doc;
 global.window = doc.defaultView;
 
 Object.keys(document.defaultView).forEach((property: string) => {
-    if (typeof global[property] === 'undefined') {
+    if (typeof global[property] === "undefined") {
         exposedProperties.push(property);
-        global[property] = document.defaultView[property] as any;
+        global[property] = document.defaultView[<any>property];
     }
 });
 
 global.navigator = {
-    userAgent: 'node.js'
+    userAgent: "node.js"
 };
