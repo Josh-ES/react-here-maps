5d6a98827d8676deee126d757b08b3dbeae7526b
Joshua Efiong <joshefiong@gmail.com>
2016-08-23 23:31:28 +0100

    Replace jsdom XMLHttpRequest implementation and link Node and jsdom consoles.

--- test/setup.ts
+++ test/setup.ts
@@ -1,13 +1,19 @@
 import * as jsdom from "jsdom";
+import { XMLHttpRequest } from "xmlhttprequest";
 
 declare var global: any;
 
 const exposedProperties = ["window", "navigator", "document"];
 
-const doc = jsdom.jsdom("<!doctype html><html><body></body></html>");
+const doc = jsdom.jsdom("<!doctype html><html><body></body></html>", {
+    virtualConsole: jsdom.createVirtualConsole().sendTo(console),
+});
 
 global.document = doc;
 global.window = doc.defaultView;
+
+// fix the XMLHttpRequest API implementation
+global.window.XMLHttpRequest = XMLHttpRequest;
 
 Object.keys(document.defaultView).forEach((property: string) => {
     if (typeof global[property] === "undefined") {
