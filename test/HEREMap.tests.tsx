11b60365b4053d716961663cd21fa2bc7249d6cc
Joshua Efiong <joshefiong@gmail.com>
2016-08-25 13:36:31 +0100

    Isolated tests, mounting HEREMap separately for each one.

--- test/HEREMap.tests.tsx
+++ test/HEREMap.tests.tsx
@@ -3,11 +3,11 @@ import cache, { getScriptStub } from "../src/utils/cache";
 import getLink from "../src/utils/get-link";
 import getScriptMap from "../src/utils/get-script-map";
 import * as chai from "chai";
-import * as cheerio from "cheerio";
 import { mount, render, shallow } from "enzyme";
 import { last, forEach } from "lodash";
 import * as React from "react";
 import * as Sinon from "sinon";
+import * as $ from "jquery";
 
 declare var global: any;
 declare var window: any;
@@ -25,24 +25,11 @@ describe("<HEREMap />", () => {
         const stylesheetUrl = 'http://js.api.here.com/v3/3.0/mapsjs-ui.css';
         getLink(stylesheetUrl, 'HERE Maps UI');
 
-        const fixture = '<div id="page-container"></div>';
+        const fixture = '<div id="page-container" style="height: 400px;"></div>';
         document.body.insertAdjacentHTML('afterbegin', fixture);
 
         getScriptStub(finalScriptToLoad).onLoad((err: any, res?: any) => {
             global.H = window.H;
-
-            const container = document.getElementById('page-container');
-
-            // need to use full DOM rendering here to access lifecycle methods
-            const wrapper = mount((
-                <HEREMap center={{ lat: 0, lng: 0 }}
-                         zoom={14}
-                         appId="NoiW7CS2CC05ppu95hyL"
-                         appCode="28L997fKdiJiY7TVVEsEGQ" />
-            ), {
-                attachTo: container,
-            });
-
             done(err);
         });
     });
@@ -50,22 +37,28 @@ describe("<HEREMap />", () => {
     it("should call componentDidMount when the component is mounted", () => {
         const didMountSpy = sinon.spy(HEREMap.prototype, "componentDidMount");
 
+        const container = document.getElementById('page-container');
+
         // need to use full DOM rendering here to access lifecycle methods
         const wrapper = mount((
             <HEREMap center={{ lat: 0, lng: 0 }}
                      zoom={14}
                      appId="NoiW7CS2CC05ppu95hyL"
                      appCode="28L997fKdiJiY7TVVEsEGQ" />
-        ));
+        ), {
+            attachTo: container,
+        });
 
         chai.expect(HEREMap.prototype.componentDidMount).to.have.property("callCount", 1);
+
         // make sure we restore the original method at the end of the test, removing the spy
         didMountSpy.restore();
+
+        // remove the test map from the DOM
+        wrapper.unmount();
     });
 
     it("should generate all the necessary script elements within the document", () => {
-        const html = document.documentElement.outerHTML;
-        const $ = cheerio.load(html);
         const scriptMap = getScriptMap();
         // check the length of the script map is equal to the number of script elements on the page
         // - we can do this as there are no other scripts on the page during testing
@@ -75,17 +68,44 @@ describe("<HEREMap />", () => {
     });
 
     it("should generate all the necessary link elements within the document", () => {
-        const html = global.document.documentElement.outerHTML;
-        const $ = cheerio.load(html);
         // check the number of link elements on the page is equal to 1
         // - we can do this as there are no other links on the page during testing
         chai.expect($("link[rel=\"stylesheet\"]").length).to.equal(1);
     });
 
     it("should generate a map when the component gets rendered", function generateMapTest() {
-        const html = document.documentElement.outerHTML;
-        const $ = cheerio.load(html);
+        const container = document.getElementById('page-container');
+
+        // need to use full DOM rendering here to access lifecycle methods
+        const wrapper = mount((
+            <HEREMap center={{ lat: 0, lng: 0 }}
+                     zoom={14}
+                     appId="NoiW7CS2CC05ppu95hyL"
+                     appCode="28L997fKdiJiY7TVVEsEGQ" />
+        ), {
+            attachTo: container,
+        });
+
         chai.expect($('canvas').length).to.equal(1);
+
+        wrapper.unmount();
+    });
+
+    it("should generate a canvas twice the size of the map container when hidpi mode is enabled", function hidpiMode() {
+        const container = document.getElementById('page-container');
+
+        // need to use full DOM rendering here to access lifecycle methods
+        const wrapper = mount((
+            <HEREMap center={{ lat: 0, lng: 0 }}
+                     zoom={14}
+                     hidpi={true}
+                     appId="NoiW7CS2CC05ppu95hyL"
+                     appCode="28L997fKdiJiY7TVVEsEGQ" />
+        ), {
+            attachTo: container,
+        });
+
+        wrapper.unmount();
     });
 
 });
