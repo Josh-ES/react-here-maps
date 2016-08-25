e15ea8dad3ebc1efef0e7a509de5ac27b0e49442
Joshua Efiong <joshefiong@gmail.com>
2016-08-25 14:42:22 +0100

    Modify getLink tests so it tests for specific link element for HERE CSS file.

--- test/utils/get-link.tests.ts
+++ test/utils/get-link.tests.ts
@@ -5,10 +5,10 @@ import * as $ from "jquery";
 declare var global: any;
 
 describe("<HEREMap />", () => {
-    describe("#getLink(url: string, name: string): any", () => {
+    describe("#getLink(url: string, name: string)", () => {
 
         it("should return a valid LinkState object", () => {
-            const linkState = getLink("http://js.api.here.com/v3/3.0/mapsjs-ui.css", "HERE Maps UI Stylesheet");
+            const linkState = getLink("http://js.api.here.com/v3/3.0/mapsjs-ui.css", "HERE Maps UI");
 
             // check that the hasLoaded property is present
             chai.expect(linkState).to.have.property("hasLoaded");
@@ -21,7 +21,7 @@ describe("<HEREMap />", () => {
         });
 
         it("should return an HTMLLinkElement as a property of a LinkState object, with the correct properties set.", () => {
-            const linkState = getLink("http://js.api.here.com/v3/3.0/mapsjs-ui.css", "HERE Maps UI Stylesheet");
+            const linkState = getLink("http://js.api.here.com/v3/3.0/mapsjs-ui.css", "HERE Maps UI");
             const { link } = linkState;
 
             // check that the href property is set to the desired URL
@@ -38,17 +38,18 @@ describe("<HEREMap />", () => {
         });
 
         it("should have a link element appended to the body of the document", () => {
-            const linkState = getLink("http://js.api.here.com/v3/3.0/mapsjs-ui.css", "HERE Maps UI Stylesheet");
+            const stylesheetUrl = "http://js.api.here.com/v3/3.0/mapsjs-ui.css";
+            const linkState = getLink(stylesheetUrl, "HERE Maps UI");
             const { link } = linkState;
 
             // check that the href property is set to the desired URL
-            chai.expect($("link").attr("href")).to.equal("http://js.api.here.com/v3/3.0/mapsjs-ui.css");
+            chai.expect($(`link[href="${stylesheetUrl}"]`).length).to.equal(1);
 
             // check that rel is set to "stylesheet"
-            chai.expect($("link").attr("rel")).to.equal("stylesheet");
+            chai.expect($(`link[href="${stylesheetUrl}"]`).attr("rel")).to.equal("stylesheet");
             
             // check that type is set to "text/css"
-            chai.expect($("link").attr("type")).to.equal("text/css");
+            chai.expect($(`link[href="${stylesheetUrl}"]`).attr("type")).to.equal("text/css");
         });
 
     });
