144dd74faf65655c611e8e4700db5fd1b7f0c16c
Joshua Efiong <joshefiong@gmail.com>
2016-08-25 20:46:05 +0100

    Remove release directory, just compile modular code to 'dist'.

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,60 @@
-var gulp = require('gulp');
 var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var tsify = require('tsify');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
 var sass = require('gulp-sass');
 var ts = require('gulp-typescript');
+var tsify = require('tsify');
+var source = require('vinyl-source-stream');
 
 var tsProject = ts.createProject('tsconfig.json', {
   typescript: require('typescript'),
 });
 
-var tsConfig = {
-  jsx: "react",
-  module: "commonjs",
-  moduleResolution: "node",
-  noImplicitAny: true,
-  target: "es5",
-  experimentalDecorators: true,
-  typescript: require("typescript"),
-};
-
 var tslint = require('gulp-tslint');
-var clean = require('gulp-clean');
 
-function tsifyBuild(entries, output, destination) {
-  return browserify({
-    basedir: '.',
-    debug: true,
-    entries: entries,
-    cache: {},
-    packageCache: {},
-  })
-  .plugin(tsify, { typescript: require('typescript') })
-  .bundle()
-  .pipe(source(output))
-  .pipe(gulp.dest(destination));
-}
+// default task
+gulp.task('default', ['transpile'], function() {
 
-gulp.task('default', function() {
-  return tsifyBuild(['src/main.ts'], 'bundle.js', 'dist');
 });
 
-gulp.task('lib', function() {
+// transpilation task from typescript to es5 javascript
+gulp.task('transpile', function() {
   return gulp.src(['src/**/*.ts', 'src/**/*.tsx', 'typings/index.d.ts', 'node_modules/typescript/lib/lib.es6.d.ts'])
     .pipe(ts(tsProject))
-    .pipe(gulp.dest('release'));
+    .pipe(gulp.dest('dist'));
 });
 
+// copies all the static files from the demo to the build directories
 gulp.task('demo-copy', function() {
   gulp.src('./demo/index.html')
     .pipe(gulp.dest('build'));
 })
 
+// transpiles all the demo scss files
 gulp.task('demo-scss', function() {
   return gulp.src(['./demo/**/*.{scss,sass}'])
     .pipe(sass({ errLogToConsole: true }))
     .pipe(gulp.dest('./build'));
 });
 
+// generates all the demo files in the build directory
 gulp.task('demo', ['default', 'demo-copy', 'demo-scss'], function() {
-  return tsifyBuild(['demo/index.tsx'], 'demo.js', 'build');
+  return browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['demo/index.tsx'],
+    cache: {},
+    packageCache: {},
+  })
+    .plugin(tsify, { typescript: require('typescript') })
+    .bundle()
+    .pipe(source('demo.js'))
+    .pipe(gulp.dest('build'));
 });
 
+// lints all the typescript source files
 gulp.task('tslint', function() {
-  return gulp.src(['src/**/*.ts', 'src/**/*.tsx', 'test/**/*.ts', 'test/**/*.tsx'])
+  return gulp.src(['./**/*.ts', './**/*.tsx', '!node_modules/**/*', '!typings/**/*'])
     .pipe(tslint({
       formatter: 'verbose',
     }))
