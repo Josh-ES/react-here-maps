5022b7252ca7ef8481556bd2770fff34db0ac609
Joshua Efiong <joshefiong@gmail.com>
2016-08-27 15:36:46 +0100

    Create utilities for getting icon instances to ensure no duplicates.

---
+++ src/utils/get-dom-marker-icon.ts
@@ -0,0 +1,21 @@
+import * as Map from "es6-map";
+
+/**
+ * Map for HTML strings against H.map.DomIcon instances
+ * @type {Map<string, ScriptState>}
+ */
+export const DomIcons = new Map<string, H.map.DomIcon>();
+
+/**
+ * Returns the DOM Icon for the input HTML string, ensuring that no more
+ * than one DOM Icon is created for each HTML string
+ * @param html {string} - A string containing the markup to be used as a Dom Icon.
+ */
+export default function getDomMarkerIcon(html: string): H.map.DomIcon {
+    if (!DomIcons.has(html)) {
+        const icon = new H.map.DomIcon(html);
+        DomIcons.set(html, icon);
+    }
+
+    return DomIcons.get(html);
+}
---
+++ src/utils/get-marker-icon.ts
@@ -0,0 +1,21 @@
+import * as Map from "es6-map";
+
+/**
+ * Map for image URL strings against H.map.Icon instances
+ * @type {Map<string, ScriptState>}
+ */
+export const Icons = new Map<string, H.map.Icon>();
+
+/**
+ * Returns the Icon for the input bitmap URL string, ensuring that no more
+ * than one Icon is created for each bitmap
+ * @param bitmap {string} - The location of the bitmap to be used as an icon
+ */
+export default function getMarkerIcon(bitmap: string): H.map.Icon {
+    if (!Icons.has(bitmap)) {
+        const icon = new H.map.Icon(bitmap);
+        Icons.set(bitmap, icon);
+    }
+
+    return Icons.get(bitmap);
+}
