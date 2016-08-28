9cd2f0e8a6804ef037372a670d7d8a20382617e6
Joshua Efiong <joshefiong@gmail.com>
2016-08-28 12:06:30 +0100

    Update demo title block to use proper HERE Map.

--- demo/containers/App.tsx
+++ demo/containers/App.tsx
@@ -4,11 +4,17 @@ import HighResolutionMap from "../examples/HighResolutionMap";
 
 import Example from "../components/Example";
 import Header from "../components/Header";
+import HEREMap from "../../src/HEREMap";
 import * as React from "react";
 
 export default class App extends React.Component<any, any> {
     public render(): JSX.Element {
         const examples = this.getExamples();
+
+        const center = {
+            lat: 55.17307,
+            lng: 15.17594,
+        };
 
         return (
             <div className="content">
@@ -18,9 +24,19 @@ export default class App extends React.Component<any, any> {
 
                 <section id="title-block">
                     <div className="container">
-                        <h1>React HERE Maps</h1>
-                        <h2>React wrapper for the HERE Maps API for JavaScript</h2>
+                        <div className="offset">
+                            <h1>React HERE Maps</h1>
+                            <h2>React wrapper for the HERE Maps API for JavaScript</h2>
+                        </div>
                     </div>
+
+                    <HEREMap
+                        zoom={7}
+                        center={center}
+                        hidpi={true}
+                        appId="NoiW7CS2CC05ppu95hyL"
+                        appCode="28L997fKdiJiY7TVVEsEGQ"
+                    />
                 </section>
 
                 { examples.map((example, index) => (
