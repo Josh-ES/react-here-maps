75f988bdb94369c2a26080fab126aee63a5005be
Joshua Efiong <joshefiong@gmail.com>
2016-10-04 12:21:37 +0100

    Initialise protocol variable to an empty string

--- src/utils/get-script-map.ts
+++ src/utils/get-script-map.ts
@@ -10,7 +10,7 @@ export function getScriptMap(secure?: boolean): ScriptMap {
     const codeVersion = "3.0";
 
     // get the relevant protocol for the HERE Maps API
-    let protocol = null;
+    let protocol = "";
 
     if (secure === true) {
         protocol = "https:";
